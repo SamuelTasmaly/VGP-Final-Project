435b401666589d92932b44315c2184cd3e24e411
SamuelTasmaly <139364@student.vansd.org>
2023-05-08 10:16:59 -0700

    added roads and city

--- Final Project/Assets/Scripts/PlayerController.cs
+++ Final Project/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     public float speed = 5000;
+    public float turnSpeed = 45.0f;
     void Start()
     {
 
@@ -16,6 +17,8 @@ public class PlayerController : MonoBehaviour
     {
         // Add force to player in direction of the focal point (and camera)
         float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
         playerRb.AddForce(transform.forward * verticalInput * speed * Time.deltaTime);
+        transform.Rotate(Vector3.up, turnSpeed *horizontalInput*Time.deltaTime);
     }
 }
