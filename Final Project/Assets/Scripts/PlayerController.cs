f0f332da941d81b2fd14752beb981aa6f4e3d7a8
SamuelTasmaly <139364@student.vansd.org>
2023-05-17 10:32:04 -0700

    improvements to vehicle control

--- Final Project/Assets/Scripts/PlayerController.cs
+++ Final Project/Assets/Scripts/PlayerController.cs
@@ -19,10 +19,15 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Add force to player in direction of the focal point (and camera)
-        float verticalInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(transform.forward * verticalInput * speed * Time.deltaTime);
-        float horizontalInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up, turnSpeed *horizontalInput*Time.deltaTime);
+        //float verticalInput = Input.GetAxis("Vertical");
+        //playerRb.AddForce(transform.forward * verticalInput * speed * Time.deltaTime);
+        //float horizontalInput = Input.GetAxis("Horizontal");
+        //transform.Rotate(Vector3.up, turnSpeed *horizontalInput*Time.deltaTime);
+        //
+        float forward = Input.GetAxis ("Vertical") * speed;
+		GetComponent<Rigidbody>().AddRelativeForce (0, 0, forward);
+		float turn = Input.GetAxis ("Horizontal") * turnSpeed;
+		this.transform.Rotate (0, turn, 0);
         
     }
 }
